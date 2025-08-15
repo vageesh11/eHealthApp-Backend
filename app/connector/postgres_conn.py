a392c5a105de43cc477053fa8067f580fadbb012
rahamath2001 <rahamathgousiya@gmail.com>
2025-08-15 15:18:42 +0530

    editing url

--- app/connector/postgres_conn.py
+++ app/connector/postgres_conn.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE = "postgresql://postgres:2323@localhost:5432/QuizApplicationYT"
+load_dotenv()
 
-engine = create_engine(URL_DATABASE)
+def database_url():
+    url=os.getenv("DATABASE_URL")
+    engine = create_engine(url)
 
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+    SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
-Base = declarative_base()
+    Base = declarative_base()
+
+    return engine,SessionLocal,Base 
+    # return "database_url function to DB successfully created"
--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routes import health, testing_routes
-from app.connector.postgres_conn import engine, SessionLocal
+from app.connector.postgres_conn import engine, SessionLocal, database_url
 from app.schema import models
 
 from app.routes import health,testing_routes
+
+engine,SessionLocal,Base=database_url()
 
 models.Base.metadata.create_all(bind=engine)
 
