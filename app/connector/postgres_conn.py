bb46b57715ae28ef9cc4d1c0af156d93180bbb08
rahamath2001 <rahamathgousiya@gmail.com>
2025-08-15 16:43:44 +0530

    Editing Changes in URL

--- app/connector/postgres_conn.py
+++ app/connector/postgres_conn.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
 load_dotenv()
