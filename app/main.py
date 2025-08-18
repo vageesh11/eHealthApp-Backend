96a13304bd4b05cdff83d9e20b39fb1c8e3b9ea6
rahamath2001 <rahamathgousiya@gmail.com>
2025-08-18 14:01:05 +0530

    Logger form

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from app.logging.logger import logger
 from app.routes import health
 
 app = FastAPI()
 
 app.include_router(health.healthRoute)
+
+@app.get("/")
+def root():
+    logger.info("Root endpoint was called")
+    return {"message":"Hello,World!"}
--- app/routes/health.py
+++ app/routes/health.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+from app.logging.logger import logger
 
 healthRoute = APIRouter()
 
 @healthRoute.get("/health")
 def health_check():
+    logger.info("Health check endpoint was called")
     return {"status": "ok"}
